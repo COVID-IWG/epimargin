90d765affd471dbe1a20378ee9fed3344a19326a
Caitlin Loftus <caitlinmloftus@gmail.com>
2020-06-01 12:30:35 -0500

    pr requested changes

--- studies/maharashtra/etl.py
+++ studies/maharashtra/etl.py
@@ -62,30 +62,3 @@ def log_delta_smoothed(time_series: pd.DataFrame) -> pd.DataFrame:
     log_delta = pd.DataFrame(np.log(time_series.cases)).rename(columns = {"cases" : "logdelta"})
     log_delta["time"] = (log_delta.index - log_delta.index.min()).days
     return log_delta
-
-# def district_migration_matrix(matrix_path: Path) -> np.matrix:
-#     mm = pd.read_csv(matrix_path)
-#     mm_state = mm[(mm.D_StateCensus2001 == "MAHARASHTRA") & (mm.O_StateCensus2001 == "MAHARASHTRA")]
-#     pivot    = mm_state.pivot(index = "D_DistrictCensus2001", columns = "O_DistrictCensus2001", values = "NSS_STMigrants").fillna(0)
-#     M  = np.matrix(pivot)
-#     Mn = M/M.sum(axis = 0)
-#     Mn[np.isnan(Mn)] = 0
-
-#     districts = [replacements.get(district, district) for district in pivot.index]
-
-#     return (districts, mm_state.groupby("O_DistrictCensus2001")["O_Population_2011"].agg(lambda x: list(x)[0]).values, Mn)
-
-
-# def migratory_influx_matrix(influx_path: Path, num_migrants: int, release_rate: float) -> Dict[str, float]:
-#     source_actives = np.array([21468, 80, 539, 461, 1007, 5291, 1893, 5254, 1595, 377, 1797, 7438, 148])
-#     source_pops    = np.array([112400000.0, 34520000.0, 61100000.0, 35190000.0, 84580000.0, 60440000.0, 68550000.0, 26500000.0, 27740000.0, 27760000.0, 199800000.0, 72150000.0, 1055000.0])
-
-#     flux = pd.read_csv(influx_path).fillna(0)
-#     flux = flux[flux.State != "Other States"] 
-#     flux = flux[flux.State != "Grand Total"] 
-#     flux = flux[[col for col in flux.columns if col not in {"State", "Unknown", "Grand Total"}]]
-#     # flux.rename(columns = {"Muzaffarpur" : "Muzzafarpur"})
-
-#     # influx proportions
-#     proportions = (flux * (source_actives/source_pops)[:, None]).sum(axis = 0)
-#     return {replacements.get(k.upper(), k.upper()): v for (k, v) in (num_migrants * release_rate * (proportions / proportions.sum())).astype(int).to_dict().items()}
