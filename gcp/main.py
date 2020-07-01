720795aff0b3815aaf3af4b04f4e06f11b30be05
manasip5 <65033326+manasip5@users.noreply.github.com>
2020-07-01 16:09:48 -0500

    Update main.py

--- gcp/main.py
+++ gcp/main.py
@@ -20,26 +20,26 @@ def timenow(event, context):
     print("""This Function was triggered by messageId {} published at {}
     """.format(context.event_id, context.timestamp))
     
-    #d= {"Time": pd.Timestamp.now()}
+    d= {"Time": pd.Timestamp.now()}
     
-    #d = ts()
-    
-    #print("Test if ts worked", d)
-    
-    #df = pd.DataFrame(data=d, index=range(1))
-
-    #df.to_csv("/tmp/test_gcp.csv", index=False)
-    
-    #print("Generated csv")
-    
-    
-
-
-if __name__ == "__main__":
     d = ts()
+    
+    print("Test if ts worked", d)
     
     df = pd.DataFrame(data=d, index=range(1))
 
     df.to_csv("/tmp/test_gcp.csv", index=False)
     
     print("Generated csv")
+    
+    
+
+
+#if __name__ == "__main__":
+#    d = ts()
+    
+#    df = pd.DataFrame(data=d, index=range(1))
+
+ #   df.to_csv("/tmp/test_gcp.csv", index=False)
+    
+  #  print("Generated csv")
