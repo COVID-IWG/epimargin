b1dee3a9558ef898e9627ef766c8349a62d43e46
manasip5 <65033326+manasip5@users.noreply.github.com>
2020-07-01 15:02:09 -0500

    Update main.py

--- gcp/main.py
+++ gcp/main.py
@@ -5,19 +5,25 @@ Dummy function to test GCP functionality
 import pandas as pd
 
 
-def timenow():
+def timenow(event, context):
     '''
     Function creates a csv file with timestamp
     
     Output:
         Returns a csv file
     '''
+    import base64
+    import pandas as pd
+    
+    print("""This Function was triggered by messageId {} published at {}
+    """.format(context.event_id, context.timestamp))
+    
     d= {"Time": pd.Timestamp.now()}
     
     df = pd.DataFrame(data=d, index=range(1))
 
-    return df.to_csv("test_gcp.csv", index=False)
+    return df.to_csv("/tmp/test_gcp.csv", index=False)
 
 
-if __name__ == "__main__":
-    timenow()
+#if __name__ == "__main__":
+#    timenow()
