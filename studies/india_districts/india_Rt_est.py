ccefb617f8f408e5a929ef9cd291527502b5018b
satej soman <satej.soman@gmail.com>
2020-07-05 00:22:44 -0500

    add np builtin kernels

--- studies/india_districts/india_Rt_est.py
+++ studies/india_districts/india_Rt_est.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from adaptive.estimators import box_filter, gamma_prior
+from adaptive.estimators import gamma_prior
+from adaptive.smoothing import convolution
 from adaptive.utils import cwd
 from adaptive.plots import plot_RR_est, plot_T_anomalies
 from etl import download_data, get_time_series, load_all_data, data_path
@@ -40,7 +41,7 @@ if __name__ == "__main__":
         T_pred, T_CI_upper, T_CI_lower,
         total_cases, new_cases_ts,
         anomalies, anomaly_dates
-    ) = gamma_prior(ts.Hospitalized[ts.Hospitalized > 0], CI = CI, smoothing = lambda ts: box_filter(ts, smoothing, None))
+    ) = gamma_prior(ts.Hospitalized[ts.Hospitalized > 0], CI = CI, smoothing = convolution("hamming", 10))
     estimates = pd.DataFrame(data = {
         "dates": dates,
         "RR_pred": RR_pred,
