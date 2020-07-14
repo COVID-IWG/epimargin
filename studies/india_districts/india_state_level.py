56af5da9579d83b133626ad7239935ba427ce401
satej soman <satej.soman@gmail.com>
2020-07-14 04:25:37 -0500

    wire up output to feed website (#68)

--- studies/india_districts/india_state_level.py
+++ studies/india_districts/india_state_level.py
@@ -1,18 +1,22 @@
 from pathlib import Path
 from typing import Dict, Optional, Sequence
+from warnings import simplefilter
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
-
-from adaptive.plots import plot_RR_est
-from adaptive.estimators import gamma_prior
-from adaptive.smoothing import convolution
-from adaptive.utils import cwd, days
-from adaptive.etl.covid19india import download_data, load_statewise_data, get_time_series
 from statsmodels.regression.linear_model import OLS
 from statsmodels.tools import add_constant
+from tqdm import tqdm
+
+from adaptive.estimators import gamma_prior
+from adaptive.etl.covid19india import (download_data, get_time_series,
+                                       load_statewise_data, state_name_lookup)
+from adaptive.plots import plot_RR_est
+from adaptive.smoothing import convolution
+from adaptive.utils import cwd, days
+
+simplefilter("ignore")
 
 def project(dates, R_values, smoothing, period = 7*days):
     julian_dates = [_.to_julian_date() for _ in dates[-smoothing//2:None]]
@@ -23,52 +27,56 @@ def project(dates, R_values, smoothing, period = 7*days):
     .fit()\
     .predict([1, julian_dates[-1] + period])[0]
 
-if __name__ == "__main__":
-    root = cwd()
-    data = root/"data"
+root = cwd()
+data = root/"data"
 
-    # model details 
-    gamma      = 0.2
-    smoothing = 15
-    CI        = 0.95
+# model details 
+gamma     = 0.2
+smoothing = 21
+CI        = 0.95
 
-    download_data(data, 'state_wise_daily.csv')
+download_data(data, 'state_wise_daily.csv')
 
-    state_df = load_statewise_data(data/"state_wise_daily.csv")
-    country_time_series = get_time_series(state_df)
+state_df = load_statewise_data(data/"state_wise_daily.csv")
+country_time_series = get_time_series(state_df)
 
-    # country level
-    (
-        dates,
-        RR_pred, RR_CI_upper, RR_CI_lower,
-        T_pred, T_CI_upper, T_CI_lower,
-        total_cases, new_cases_ts,
-        anomalies, anomaly_dates
-    ) = gamma_prior(country_time_series["Hospitalized"].iloc[:-1], CI = CI, smoothing = convolution(window = smoothing)) 
+estimates  = []
+timeseries = []
 
-    plot_RR_est(dates, RR_pred, RR_CI_upper, RR_CI_lower, CI, ymin=0, ymax=4)\
-        .title("India: Reproductive Number Estimate")\
-        .xlabel("Date")\
-        .ylabel("Rt", rotation=0, labelpad=20)
-    plt.ylim(0, 4)
-    plt.show()
+# country level
+(
+    dates,
+    RR_pred, RR_CI_upper, RR_CI_lower,
+    T_pred, T_CI_upper, T_CI_lower,
+    total_cases, new_cases_ts,
+    anomalies, anomaly_dates
+) = gamma_prior(country_time_series["Hospitalized"].iloc[:-1], CI = CI, smoothing = convolution(window = smoothing)) 
 
-    # state level rt estimates
-    state_time_series = get_time_series(state_df, 'state')
-    state_names = list(state_time_series.index.get_level_values(level=0).unique())
-    smooth = 10
-    estimates = []
-    max_len = 1 + max(map(len, state_names))
-    with tqdm(state_names) as states:
-    	for state in states:
-    		states.set_description(f"{state :<{max_len}}")
-    		try: 
-    			(dates, RR_pred, RR_CI_upper, RR_CI_lower, T_pred, T_CI_upper, T_CI_lower, total_cases, new_cases_ts, anomalies, anomaly_dates) = gamma_prior(state_time_series.loc[state]['Hospitalized'], CI = CI, smoothing = convolution(window = smooth))
-    			estimates.append((state, RR_pred[-1], RR_CI_lower[-1], RR_CI_upper[-1], project(dates, RR_pred, smooth))) 
-    		except (IndexError, ValueError): 
-    			estimates.append((state, np.nan, np.nan, np.nan, np.nan))
-    estimates = pd.DataFrame(estimates)
-    estimates.columns = ["state", "Rt", "Rt_CI_lower", "Rt_CI_upper", "Rt_proj"]
-    estimates.set_index("state", inplace=True)
-    estimates.to_csv(data/"Rt_estimates.csv")
-    print(estimates)
+country_code = state_name_lookup["India"]
+for row in zip(dates, RR_pred, RR_CI_upper, RR_CI_lower):
+    timeseries.append((country_code, *row))
+
+# state level rt estimates
+state_time_series = get_time_series(state_df, 'state')
+state_names = list(state_time_series.index.get_level_values(level=0).unique())
+max_len = 1 + max(map(len, state_names))
+with tqdm(state_names) as states:
+    for state in states:
+        state_code = state_name_lookup[state]
+        states.set_description(f"{state :<{max_len}}")
+        try: 
+            (dates, RR_pred, RR_CI_upper, RR_CI_lower, T_pred, T_CI_upper, T_CI_lower, total_cases, new_cases_ts, anomalies, anomaly_dates) = gamma_prior(state_time_series.loc[state]['Hospitalized'], CI = CI, smoothing = convolution(window = smoothing))
+            for row in zip(dates, RR_pred, RR_CI_upper, RR_CI_lower):
+                timeseries.append((state_code, *row))
+            estimates.append((state_code, RR_pred[-1], RR_CI_lower[-1], RR_CI_upper[-1], project(dates, RR_pred, smoothing)))
+        except (IndexError, ValueError): 
+            estimates.append((state, np.nan, np.nan, np.nan, np.nan))
+# estimates = pd.DataFrame(estimates)
+# estimates.columns = ["state", "Rt", "Rt_CI_lower", "Rt_CI_upper", "Rt_proj"]
+# estimates.set_index("state", inplace=True)
+# estimates.to_csv(data/"Rt_estimates.csv")
+timeseries = pd.DataFrame(timeseries)
+timeseries.columns = ["state", "date", "Rt", "Rt_upper", "Rt_lower"]
+timeseries.set_index("state", inplace=True)
+timeseries.to_csv(data/"Rt_timeseries_india.csv")
+print(timeseries)
