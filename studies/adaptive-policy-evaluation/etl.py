75b1e57811f919b3b3a1ad9f68a1d4492e8e3a6a
Caitlin Loftus <caitlinmloftus@gmail.com>
2020-09-07 18:12:30 -0500

    data pipeline cleaned and all updated for metro-state aggregations

--- studies/adaptive-policy-evaluation/etl.py
+++ studies/adaptive-policy-evaluation/etl.py
@@ -64,44 +64,21 @@ state_name_lookup = {
     'WY': 'Wyoming'
 }
 
-county_mask_cols = [
- 'state_fips', 
- 'state_name',
- 'county_fips',
- 'county_name',
- 'county_mask_policy_start',
- 'state_mask_policy_end',
- 'county_conditions', 
- 'state_mask_policy_start', 
- 'state_conditions'
- ]
-
 google_mobility_cols = [
-    "retail_and_recreation_percent_change_from_baseline", 
-    "grocery_and_pharmacy_percent_change_from_baseline",
-    "parks_percent_change_from_baseline", 
-    "transit_stations_percent_change_from_baseline", 
-    "workplaces_percent_change_from_baseline", 
-    "residential_percent_change_from_baseline"
+    'retail_and_recreation_percent_change_from_baseline', 
+    'grocery_and_pharmacy_percent_change_from_baseline',
+    'parks_percent_change_from_baseline', 
+    'transit_stations_percent_change_from_baseline', 
+    'workplaces_percent_change_from_baseline', 
+    'residential_percent_change_from_baseline'
 ]
-
-# Added dummy_cols
-dummy_cols = [
-    'intervention_>50_gatherings',
-    'intervention_>500_gatherings', 
-    'intervention_Federal_guidelines',
-    'intervention_entertainment/gym', 
-    'intervention_foreign_travel_ban', 
-    'intervention_public_schools',
-    'intervention_restaurant_dine-in', 
-    'intervention_stay_at_home']
 
 # Added columns that have max data between mar-jul
 data_cols = [
-    "retail_and_recreation_percent_change_from_baseline",
-    "grocery_and_pharmacy_percent_change_from_baseline",
-    "workplaces_percent_change_from_baseline",
-    "residential_percent_change_from_baseline"
+    'retail_and_recreation_percent_change_from_baseline',
+    'grocery_and_pharmacy_percent_change_from_baseline',
+    'workplaces_percent_change_from_baseline',
+    'residential_percent_change_from_baseline'
 ]
 
 state_interventions_info = {
@@ -126,90 +103,53 @@ state_meta_data = {
  }
 
 def load_country_google_mobility(country_code: str) -> pd.DataFrame:
-    cols = ["sub_region_1","census_fips_code","date"] + google_mobility_cols
-    full_df = pd.read_csv('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv', parse_dates = ["date"])
-    country_df = full_df[full_df["country_region_code"] == country_code]
-    country_df["sub_region_1"] = country_df["sub_region_1"].str.replace(" and ", " & ")
+    cols = ['sub_region_1','census_fips_code','date'] + google_mobility_cols
+    full_df = pd.read_csv('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv', parse_dates = ['date'])
+    country_df = full_df[full_df['country_region_code'] == country_code]
+    country_df['sub_region_1'] = country_df['sub_region_1'].str.replace(' and ', ' & ')
     return country_df[cols]
 
-def load_us_county_data(file: str, url: Optional[str] = "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/") -> pd.DataFrame:
+def load_us_county_data(file: str, url: Optional[str] = 'https://usafactsstatic.blob.core.windows.net/public/data/covid-19/') -> pd.DataFrame:
     df = pd.read_csv(url + file, dtype={'countyfips': str})
-    df.columns = df.columns.str.lower().str.replace(" ", "_")
-    df["state_name"] = df["state"].map(state_name_lookup)
-    df["county_name"] = df["county_name"].str.title()
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
+    df['state_name'] = df['state'].map(state_name_lookup)
+    df['county_name'] = df['county_name'].str.title()
     return df
-
-def load_intervention_data() -> pd.DataFrame:
-    interventions = pd.read_csv('https://raw.githubusercontent.com/JieYingWu/COVID-19_US_County-level_Summaries/master/raw_data/national/public_implementations_fips.csv')
-    interventions.rename(columns={"Unnamed: 1": "county_name", "Unnamed: 2": "state", "FIPS": "countyfips"}, inplace=True)
-    interventions["county_name"] = interventions["county_name"].str.title()
-    interventions = pd.DataFrame(interventions.set_index(["state", "countyfips"]).iloc[:, 1:].stack(), columns=["date"]).reset_index().rename(columns={"level_2": "intervention"})    
-    interventions["state_name"] = interventions["state"].map(state_name_lookup)
-    interventions["date"] = pd.to_datetime(interventions["date"]+ "-2020", format="%d-%b-%Y")
-    interventions_dummy = pd.get_dummies(interventions.set_index(["state_name", "countyfips", "date"]).iloc[:,1:].sort_index()).reset_index()
-    interventions_dummy.columns = [x.replace(" ", "_" ) for x in interventions_dummy.columns]
-    return interventions_dummy.groupby(["state_name", "countyfips", "date"])[dummy_cols].sum()
-
-def load_rt_estimations(data_path: Path) -> pd.DataFrame:
-    rt_df = pd.read_csv(data_path, parse_dates = ["date"])
-    rt_df["state_name"] = rt_df["state"].map(state_name_lookup)
-    return rt_df.iloc[:, 1:].set_index(["state_name", "date"])
-
-def load_county_mask_data(data_path: Path) -> pd.DataFrame:
-    # county_mask_policy_end is currently too messy to use, but we should check if it is usable in future
-    mask_df = pd.read_csv(data_path, parse_dates=["county_mask_policy_start", "state_mask_policy_start", "state_mask_policy_end"], usecols=county_mask_cols)
-    for col in ["_mask_policy_start", "_conditions"]:
-        mask_df["county" + col].fillna(mask_df["state" + col], inplace=True)
-    mask_df[['state_name']] = mask_df['state_name'].str.title()
-    mask_df = mask_df[mask_df['county_fips'] != 'Yes'] # messy data joys!
-    mask_df['countyfips'] = pd.to_numeric(mask_df['county_fips'])
-    # for now just including "all public places" mask policies as the other variations are complicated 
-    return mask_df[mask_df["county_conditions"] == "all public places"]
 
 def load_metro_areas(data_path: Path) -> pd.DataFrame:
     metro_df = pd.read_csv(data_path)
-    metro_df.rename(columns={"county_fips": "countyfips"}, inplace=True)
-    return metro_df[metro_df["area_type"] == "Metro"][["cbsa_fips", "countyfips", "county_name", "state_codes", "state_name"]]
+    metro_df.rename(columns={'county_fips': 'countyfips'}, inplace=True)
+    return metro_df[metro_df['area_type'] == 'Metro'][['cbsa_fips', 'countyfips', 'county_name', 'state_codes', 'state_name']]
 
-def fill_dummies(grp, intervention_col, start_date_col, g):
-    start_date = grp[start_date_col].unique()
-    end_date = grp[end_date_col].unique()
+def fill_dummies(grp, intervention_col: str, start_date_col: str, end_date_col: Optional[str] = None):
     grp[intervention_col] = 0
+    start_date = grp[start_date_col].unique()
+    end_date = grp[end_date_col].unique() if end_date_col else [None]
     if start_date[0]:
         if end_date[0]:
             grp.loc[(grp.index.get_level_values(level = 'date') >= start_date[0]) & (grp.index.get_level_values(level='date') <= end_date[0]),intervention_col] = 1
         grp.loc[(grp.index.get_level_values(level = 'date') >= start_date[0]),intervention_col] = 1
     return grp 
-
-def add_mask_dummies(grp, mask_df):
-    start_date = mask_df["county_mask_policy_start"].values[mask_df["countyfips"] == grp.name[1]]
-    end_date = mask_df["state_mask_policy_end"].values[mask_df["countyfips"] == grp.name[1]]
-    if not start_date.size == 0:
-        if not end_date.size == 0:
-            grp.loc[(grp.index.get_level_values(level='date') >= start_date[0]) & (grp.index.get_level_values(level='date') <= end_date[0]), "intervention_mask_all_public"] = 1
-        else:
-            grp.loc[(grp.index.get_level_values(level='date') >= start_date[0]), "intervention_mask_all_public"] == 1
-    return grp
     
 def state_level_intervention_data(data_path: Path) -> pd.DataFrame:
     state_policy_df = pd.read_excel(data_path, 1)[list(state_interventions_info.keys())]
-    state_policy_df = state_policy_df.set_index("STATE").iloc[4:, :].dropna(axis=0)
+    state_policy_df = state_policy_df.set_index('STATE').iloc[4:, :].dropna(axis=0)
     state_policy_df.columns = ['state','start_stay_at_home','end_stay_at_home','religious_exception_stay_home','mask_mandate_all']
     return state_policy_df
 
 def get_case_timeseries(case_df: pd.DataFrame) -> pd.DataFrame:
-    county_cases = pd.DataFrame(case_df.set_index(["state_name","countyfips"]).iloc[:,3:].stack()).rename(columns={0:"cumulative_confirmed_cases"}).reset_index()
-    county_cases["date"] = pd.to_datetime(county_cases["level_2"],format="%m/%d/%y")
-    county_cases = county_cases.set_index(["state_name", "countyfips", "date"]).iloc[:,1:]
-    county_cases["cumulative_confirmed_cases"] = pd.to_numeric(county_cases["cumulative_confirmed_cases"])
-    county_cases = county_cases.groupby(["state_name","countyfips"]).apply(add_delta_col)
-    county_cases["daily_confirmed_cases"].fillna(county_cases["cumulative_confirmed_cases"], inplace=True)
-    return pd.DataFrame(county_cases["daily_confirmed_cases"][county_cases.index.get_level_values(level=1) != 0]).reset_index()
+    county_cases = pd.DataFrame(case_df.set_index(['state_name','countyfips']).iloc[:,3:].stack()).rename(columns={0:'cumulative_confirmed_cases'}).reset_index()
+    county_cases['date'] = pd.to_datetime(county_cases['level_2'],format='%m/%d/%y')
+    county_cases = county_cases.set_index(['state_name', 'countyfips', 'date']).iloc[:,1:]
+    county_cases['cumulative_confirmed_cases'] = pd.to_numeric(county_cases['cumulative_confirmed_cases'])
+    county_cases = county_cases.groupby(['state_name','countyfips']).apply(add_delta_col)
+    county_cases['daily_confirmed_cases'].fillna(county_cases['cumulative_confirmed_cases'], inplace=True)
+    return pd.DataFrame(county_cases['daily_confirmed_cases'][county_cases.index.get_level_values(level=1) != 0]).reset_index()
 
 def start_outbreak_dummy(case_df: pd.DataFrame) -> pd.DataFrame:
-    start_dates = case_df.reset_index().groupby(['cbsa_fips']).apply(lambda x: x['date'][x['daily_confirmed_cases'] >= 10].min())
-    case_df = case_df.reset_index().merge(start_dates.reset_index(), on='cbsa_fips', how='outer').rename(columns={0:'metro_outbreak_start'}).set_index(['cbsa_fips','state_name', 'date'])
-    return case_df.groupby(['cbsa_fips', 'state_name']).apply(fill_outbreak_dummy)
+    start_dates = case_df.reset_index().groupby(['metro-state']).apply(lambda x: x['date'][x['daily_confirmed_cases'] >= 10].min())
+    case_df = case_df.reset_index().merge(start_dates.reset_index(), on='metro-state', how='outer').rename(columns={0:'metro_outbreak_start'}).set_index(['metro-state', 'date'])
+    return case_df.groupby(['metro-state']).apply(fill_outbreak_dummy)
 
 def fill_outbreak_dummy(grp):
     start_date = grp['metro_outbreak_start'].unique()
@@ -222,7 +162,7 @@ def fill_outbreak_dummy(grp):
 
 def get_top_metros(county_populations: pd.DataFrame, metros, n: Optional[int] = 100) -> pd.DataFrame:
     metro_areas = metros.merge(county_populations[['countyfips','population']], on='countyfips')
-    metro_areas = pd.DataFrame(metro_areas.groupby('cbsa_fips')['population'].sum()).reset_index()
+    metros = pd.DataFrame(metro_areas.groupby('cbsa_fips')['population'].sum()).reset_index().merge(metro_areas[['cbsa_fips','state']], on='cbsa_fips')
     top_n_metros = list(metro_areas.sort_values(by='population', ascending=False).iloc[:n,:]['cbsa_fips'])
     return metro_areas[['countyfips','state','cbsa_fips']][metro_areas['cbsa_fips'].isin(top_n_metros)]
 
@@ -237,44 +177,48 @@ def add_lag_cols(grp: pd.DataFrame, cols: Sequence[str]):
             grp[col + '_lag_' + str(lag)] = grp[col].shift(lag)
     return grp
 
+def metro_state_mobility_agg(county_mobility: pd.DataFrame) -> pd.DataFrame:
+    return county_mobility.groupby(['metro-state', 'date']).apply(weighted, google_mobility_cols)
+
+def weighted(grp, cols):
+    return pd.Series(np.average(grp[cols], weights=grp['pop_prop'], axis=0), cols)
+
 def pop_prop_col(grp):
     grp['pop_prop'] = grp['population'] / grp['population'].sum()
     return grp
 
 def add_delta_col(grp: pd.DataFrame):
-    grp["daily_confirmed_cases"] = grp["cumulative_confirmed_cases"].diff()
+    grp['daily_confirmed_cases'] = grp['cumulative_confirmed_cases'].diff()
     return grp
 
 def poli_aff(data_path: Path) -> pd.DataFrame:
     vote_df = pd.read_csv(data_path)
     vote16_df = vote_df[vote_df.year==2016]
     # Format data
-    vote16_df = vote16_df.reset_index(drop=True).fillna(value={"party": "other"})
-    vote16_df.drop(columns = ["year", "office", "version"], inplace=True)
-    vote16_df["pvote"] = vote16_df.candidatevotes / vote16_df.totalvotes
+    vote16_df = vote16_df.reset_index(drop=True).fillna(value={'party': 'other'})
+    vote16_df.drop(columns = ['year', 'office', 'version'], inplace=True)
+    vote16_df['pvote'] = vote16_df.candidatevotes / vote16_df.totalvotes
     # Create pivot table
-    tbl_vote16 = pd.pivot_table(vote16_df, values="pvote", index=["state", "state_po", "county", "FIPS"], columns="party", aggfunc=np.sum).reset_index()
-    tbl_vote16.rename(columns={"FIPS": "countyfips"}, inplace=True)
+    tbl_vote16 = pd.pivot_table(vote16_df, values='pvote', index=['state', 'state_po', 'county', 'FIPS'], columns='party', aggfunc=np.sum).reset_index()
+    tbl_vote16.rename(columns={'FIPS': 'countyfips'}, inplace=True)
     #Create indicator variables
-    tbl_vote16["dem_ind"] = tbl_vote16.democrat.apply(lambda x: 0 if x <0.5 else 1)
-    tbl_vote16["rep_ind"] = tbl_vote16.republican.apply(lambda x: 0 if x < 0.5 else 1)
-    tbl_vote16["oth_ind"] = tbl_vote16.other.apply(lambda x: 0 if x < 0.5 else 1)
+    tbl_vote16['dem_ind'] = tbl_vote16.democrat.apply(lambda x: 0 if x <0.5 else 1)
+    tbl_vote16['rep_ind'] = tbl_vote16.republican.apply(lambda x: 0 if x < 0.5 else 1)
+    tbl_vote16['oth_ind'] = tbl_vote16.other.apply(lambda x: 0 if x < 0.5 else 1)
     return tbl_vote16
 
 def impute_missing_mobility(county_df: pd.DataFrame) -> pd.DataFrame:
     county_df.reset_index(inplace=True)
-    #Drop 133 counties - refer data analysis
-    #county_remain = county_df[~county_df.countyfips.isin(drop_county)]
     for county in county_df.countyfips.unique():
         for col in google_mobility_cols:
             try:
-                county_df.loc[county_df.countyfips==county, col] = county_df.loc[county_df.countyfips==county, col].interpolate(method="cubic", limit_direction="both", limit_area="inside")
+                county_df.loc[county_df.countyfips==county, col] = county_df.loc[county_df.countyfips==county, col].interpolate(method='cubic', limit_direction='both', limit_area='inside')
             except:
                 continue
     return county_df
 
 def remain_county(county_df: pd.DataFrame, pct_data: Optional[float] = 0.4, mth: Optional[list] = [3,4,5,6,7]) -> pd.DataFrame:
-    county_df["month"] = county_df.date.apply(lambda x: x.month)
+    county_df['month'] = county_df.date.apply(lambda x: x.month)
     month_df = county_df[county_df.month.isin(mth)].reset_index(drop=True)
     cnt_df = month_df.groupby(['countyfips', 'population'])[data_cols].count().reset_index()
     county_ls_total = []    
--- studies/adaptive-policy-evaluation/policy_data_pipeline.py
+++ studies/adaptive-policy-evaluation/policy_data_pipeline.py
@@ -8,66 +8,56 @@ from etl import *
 from adaptive.utils import cwd, days
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     root = cwd()
-    data = root/"data"
-    figs = root/"figs"
+    data = root/'data'
+    figs = root/'figs'
 
     # load meta data for metro-state aggregations & filter to top 100 metro areas
-    county_populations = load_us_county_data("covid_county_population_usafacts.csv")
+    county_populations = load_us_county_data('covid_county_population_usafacts.csv')
     county_populations = county_populations[county_populations['county_name'] != 'Statewide Unallocated']
-    metro_areas = load_metro_areas(data/"county_metro_state_walk.csv").rename(columns={"state_codes": "state", "county_fips": "countyfips"})
+    metro_areas = load_metro_areas(data/'county_metro_state_walk.csv').rename(columns={'state_codes': 'state', 'county_fips': 'countyfips'})
     top_metros = get_top_metros(county_populations, metro_areas)
 
+    top_metros = county_populations.merge(top_metros[['countyfips','cbsa_fips']], on='countyfips')
+    top_metros = top_metros.groupby(['cbsa_fips', 'state']).apply(pop_prop_col)
+    top_metros['metro-state'] = top_metros['cbsa_fips'].astype(str) + '_' + top_metros['state']
+
     # load county level daily case data 
-    case_df = load_us_county_data("covid_confirmed_usafacts.csv")
+    case_df = load_us_county_data('covid_confirmed_usafacts.csv')
     county_case_ts = get_case_timeseries(case_df).merge(top_metros, on='countyfips', how='inner')
 
-    # load county level google mobility data, and impute missing data
+    # load county level google mobility data, and impute/drop missing data
     ## N.B parks_percent_change_from_baseline and transit_stations_percent_change_from_baseline still have a lot of missing --> drop these columns from analysis
-    us_mobility = load_country_google_mobility("US").rename(columns={"sub_region_1": "state_name", "census_fips_code": "countyfips"})
-    county_mobility_ts = us_mobility[~(us_mobility["countyfips"].isna())].set_index(["countyfips", "date"])
+    us_mobility = load_country_google_mobility('US').rename(columns={'sub_region_1': 'state_name', 'census_fips_code': 'countyfips'})
+    county_mobility_ts = us_mobility[~(us_mobility['countyfips'].isna())].set_index(['countyfips', 'date'])
     county_mobility_ts = impute_missing_mobility(county_mobility_ts)
     county_mobility_imputed, lst_cnt = remain_county(county_mobility_ts.merge(county_populations[['countyfips','population']], on='countyfips'))
 
-    
-    # county level
+    # metro-state level aggregation
+    metro_state_cases = pd.DataFrame(county_case_ts.groupby(['metro-state', 'date'])['daily_confirmed_cases'].sum())
+    metro_state_mobility = metro_state_mobility_agg(county_mobility_imputed.merge(top_metros, on='countyfips', how='inner'))
 
-    county_df = county_case_ts.join(county_mobility_ts).join(county_populations.set_index(['state_name','countyfips'])).reset_index().set_index(["state_name", "countyfips", "date"])        
+    # load rt daily data
+    metro_state_rt = pd.read_csv(data/'+rt_estimates_comparison.csv', parse_dates=['date']).iloc[:,1:].rename(columns={'cbsa_fips_state':'metro-state'}).set_index(['metro-state','date'])
 
+    # create metro-state aggregated df
+    metro_state_df = metro_state_mobility.join(metro_state_cases.join(metro_state_rt)).join(top_metros[['metro-state','state', 'cbsa_fips']].drop_duplicates().set_index('metro-state'))
 
-    county_case_ts = county_case_ts.join(metro_areas.set_index(["state_name", "countyfips"])["cbsa_fips"])
-    metro_state_cases = pd.DataFrame(county_case_ts.groupby(['cbsa_fips','state_name','date'])['daily_confirmed_cases'].sum())
+    # add state level intervention dummies
+    state_interventions = state_level_intervention_data(data/'COVID-19 US state policy database_08_03_2020.xlsx').reset_index().rename(columns={'STATE':'state_name'})
+    metro_state_df = metro_state_df.reset_index().merge(state_interventions, on='state').set_index(['metro-state', 'date'])
+    metro_state_df = metro_state_df.groupby(['metro-state']).apply(fill_dummies, 'stay_at_home', 'start_stay_at_home', 'end_stay_at_home')
+    metro_state_df = metro_state_df.groupby(['metro-state']).apply(fill_dummies, 'mask_mandate', 'mask_mandate_all')
 
-    county_mobility_ts = county_mobility_ts.join(metro_areas.set_index(["state_name", "countyfips"]))
-
-    metro_state_mobility = county_mobility_ts.groupby(['cbsa_fips','state_name','date']).mean() # need to make this weighted by population
-
-    metro_state_df = metro_state_cases.join(metro_state_mobility, how='inner').sort_index()
-
-    state_interventions = state_level_intervention_data(data/"COVID-19 US state policy database_08_03_2020.xlsx").reset_index().rename(columns={'STATE':'state_name'})
-    metro_state_df = metro_state_df.join(state_interventions.set_index('state_name'))
-
-    metro_state_df = metro_state_df.groupby(['cbsa_fips','state_name']).apply(fill_dummies, 'stay_at_home', 'start_stay_at_home', 'end_stay_at_home')
-    metro_state_df = metro_state_df.groupby(["cbsa_fips","state_name"]).apply(add_lag_cols, ["daily_confirmed_cases"])
-    
     # add dummy to only include places once their outbreak has started - seems that 10 cases is the threshold used 
     metro_state_df = start_outbreak_dummy(metro_state_df)
 
-    # filter to top 100 metro areas
-    county_df_top_metros = filter_top_metros(county_df)
+    # add lagged dependent variable cols 
+    metro_state_df = metro_state_df.groupby(['metro-state']).apply(add_lag_cols, ['daily_confirmed_cases'])
 
+    # add dummies for metro areas
+    metro_state_df = get_metro_dummies(metro_state_df)
 
-    # county_interventions = interventions[~(interventions.index.get_level_values(0) == interventions.index.get_level_values(1))]
-    # county_mask_policy = load_county_mask_data(data/"county_mask_policy.csv")
-
-    # political affiliation
-    # vote_df = poli_aff(data/"countypres_2000_2016.csv").rename(columns={'state':'state_name'}).set_index(['state_name', 'countyfips'])
-    # county_df_top_metros = county_df_top_metros.join(vote_df)
-
-    # impute missing mobility data
-
-
-    county_df_top_metros_remain.to_csv(data/"county_level_policy_evaluation.csv", index=False)
-
+    metro_state_df.to_csv(data/'metro_state_policy_evaluation.csv')
