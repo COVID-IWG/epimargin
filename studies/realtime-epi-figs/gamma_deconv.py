0ccc86e7a7f45b6717111956e6145f28afc0fb56
satej soman <satej.soman@gmail.com>
2020-09-28 03:13:29 -0500

    add generation interval curves and fix deconv

--- studies/realtime-epi-figs/gamma_deconv.py
+++ studies/realtime-epi-figs/gamma_deconv.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-import np as np
+import numpy as np
 import seaborn as sns
 import tikzplotlib
-from scipy.signal import convolve
+from scipy.signal import convolve, deconvolve
 from scipy.stats import gamma, logistic, poisson
 from scipy.optimize import minimize 
 
@@ -19,25 +19,13 @@ color = [0.8423298817793848, 0.8737404427964184, 0.7524954030731037]
 
 palette = [[0.8423298817793848, 0.8737404427964184, 0.7524954030731037], [0.5815252468131623, 0.7703468311289211, 0.5923205247665932], [0.35935359003014994, 0.6245622005326175, 0.554154071059354], [0.25744332683867743, 0.42368146872794976, 0.5191691971789514], [0.21392162678343224, 0.20848424698401846, 0.3660805512579508]]
 
- 
- # http://freerangestats.info/blog/2020/07/18/victoria-r-convolution
-def deconv(observed, kernel): 
-    k = len(kernel)
-    padded = np.r_[np.zeros(k), observed]    
-    def error(x):
-        return sum(convolve(x, kernel, mode = "full")[:len(x)] - padded) ** 2
-
-    res = minimize(error, np.r_[observed, np.zeros(k)], method = "L-BFGS-B")
-
-    plt.plot(observed)
-    plt.plot(res.x)
-    plt.show()
 
 a = 5
 t = np.linspace(0, 200)
 f = 2000 * logistic.cdf((t - 75)/10)
 orig = np.r_[np.zeros(50), f, f[-1] * np.ones(50), np.flip(f)]
-pmf  = gamma.pdf(np.linspace(gamma.ppf(0.005, a), gamma.ppf(1-0.005, a)), a)/sum(pmf)
+pmf  = gamma.pdf(np.linspace(gamma.ppf(0.005, a), gamma.ppf(1-0.005, a)), a)
+pmf/= sum(pmf)
 obs  = convolve(orig, pmf, mode = "full")
 obs *= sum(orig)/sum(obs)
 
@@ -48,11 +36,11 @@ plt.ylabel("cases")
 plt.legend()
 print(tikzplotlib.get_tikz_code())
 
-b = 3
-orig = np.r_[0, 4, 6, 9, 7, 5, np.zeros(14)]
-pmf = poisson.pmf(range(9), b)
-plt.plot(pmf)
-plt.show()
+# b = 3
+# orig = np.r_[0, 4, 6, 9, 7, 5, np.zeros(14)]
+# pmf = poisson.pmf(range(9), b)
+# plt.plot(pmf)
+# plt.show()
 
 blur = convolve(orig, pmf, mode = "full")
 plt.plot(orig)
@@ -60,12 +48,20 @@ plt.plot(blur)
 
 plt.show()
 
-t = np.linspace(0, 150)
-S = 2000 * logistic.cdf((t - 75)/10)
-g = gamma.pdf(np.linspace(gamma.ppf(0.005, a), gamma.ppf(1-0.005, a)), a)
-# f = np.ones(5)/5
-# S = convolve(I, f)
+# http://freerangestats.info/blog/2020/07/18/victoria-r-convolution
+def deconv(observed, kernel): 
+    k = len(kernel)
+    padded = np.r_[np.zeros(k), observed]
+    def error(x):
+        return sum(convolve(x, kernel, mode="same")[:len(padded)] - padded) ** 2
+    
+    res = minimize(error, np.r_[observed, np.zeros(k)], method = "L-BFGS-B")
+    return res.x
 
-# plt.plot(t, S[-len(t):], color = color)
-plt.plot(t, I, color = "black")
+
+I_deconv, _ = deconvolve(obs, pmf)
+# plt.plot(orig, label = "original")
+plt.plot(obs, label = "observed")
+plt.plot(I_deconv, label = "deconvolved")
+plt.legend()
 plt.show()
