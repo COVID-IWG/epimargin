b975aa6c193f1716d3454a8fda27af64285b868b
satej soman <satej.soman@gmail.com>
2021-01-30 17:51:32 -0600

    state-level vax policies

--- studies/age_structure/vaccination_policies.py
+++ studies/age_structure/vaccination_policies.py
@@ -11,6 +11,8 @@ from adaptive.etl.covid19india import state_code_lookup
 from adaptive.models import SIR
 from adaptive.smoothing import notched_smoothing
 
+from scipy.stats import multinomial as Multinomial
+
 sns.set(style = "whitegrid")
 
 data = Path("./data").resolve()
@@ -82,6 +84,15 @@ D = df[state].loc[simulation_start, "total", "deceased"]
 (Rt_dates, Rt_est, *_) = analytical_MPVS(T_ratio * dT_conf_smooth, CI = CI, smoothing = lambda _:_, totals = False)
 Rt = dict(zip(Rt_dates, Rt_est))
 
+def random_assignment(daily_rate: float, vax_effectiveness: float) -> np.array:
+    pass 
+
+vax_pct_annual_goal = 0.50
+daily_rate = vax_pct_annual_goal/365
+vax_effectiveness = 1.00
+daily_vax_doses = int(vax_effectiveness * daily_rate * N)
+immunity_threshold = 0.75
+
 model = SIR(
     name        = state, 
     population  = N, 
@@ -92,56 +103,70 @@ model = SIR(
     D0          = np.ones(num_sims) * D,
     random_seed = 0
 )
-i = 0
 
-Vf = 0.01
+t = 0
+dVx = [np.zeros(len(IN_age_structure))]
 
-while np.mean(model.dT[-1]) > 0:
-    model.parallel_forward_epi_step(num_sims = num_sims)
-    i += 1
-    print(state, i, np.mean(model.dT[-1]), np.std(model.dT[-1]))
+# run vax rate forward 1 year, then until 75% of pop is recovered or vax
+while (t <= 365) or ((t > 365) and (model.R[-1].mean() + (daily_vax_doses * t))/N < immunity_threshold):
+    dVx.append(Multinomial.rvs(daily_vax_doses, IN_age_ratios))
+    model.S[-1] -= daily_vax_doses
+    model.parallel_forward_epi_step()
+    print(state, t, np.mean(model.dT[-1]), np.std(model.dT[-1]))
+    t += 1
+
+parameter_tag = f"{state}_randomassignment_ve{int(100*vax_effectiveness)}_annualgoal{int(100 * vax_pct_annual_goal)}_threshold{int(100 * immunity_threshold)}"
+pd.DataFrame(dVx)\
+    .to_csv(data/f"dVx_{parameter_tag}.csv")
+pd.DataFrame([split_by_age(_.mean()).astype(int) for _ in model.R])\
+    .to_csv(data/f"Rx_{parameter_tag}.csv")
+
+# while np.mean(model.dT[-1]) > 0:
+#     model.parallel_forward_epi_step(num_sims = num_sims)
+#     i += 1
+#     print(state, i, np.mean(model.dT[-1]), np.std(model.dT[-1]))
 
 # plot simulation
-plt.scatter(dT_conf["April 1, 2020":simulation_start].index, dT_conf["April 1, 2020":simulation_start].values*T_ratio,  label = "seroprevalence-scaled cases (pre-simulation)", color = "black", s = 5)
-dates = pd.date_range(simulation_start, simulation_start + pd.Timedelta(len(model.dT) - 1, "days"))
-n = len(dates)
-plt.plot(dates, np.array([_.mean().astype(int) for _ in model.dT][:n]), label = "mean simulated daily cases", color = "rebeccapurple")
-plt.fill_between(dates, [_.min().astype(int) for _ in model.dT][:n], [_.max().astype(int) for _ in model.dT][:n], label = "simulation range", alpha = 0.3, color = "rebeccapurple")
-plt.vlines(pd.Timestamp(date), 1, 1e6, linestyles = "dashed", label = "date of seroprevalence study")
-plt.legend(handlelength = 1, framealpha = 1)
-plt.semilogy()
-plt.xlim(pd.Timestamp("April 1, 2020"), dates[-1])
-plt.ylim(1, 1e6)
-plt.PlotDevice().xlabel("\ndate").ylabel("new daily cases\n").annotate(f"Daily Cases: Scaled Data & Simulation - Tamil Nadu (100% ve, {100 * Vf}% random assignment)")
-plt.show()
+# plt.scatter(dT_conf["April 1, 2020":simulation_start].index, dT_conf["April 1, 2020":simulation_start].values*T_ratio,  label = "seroprevalence-scaled cases (pre-simulation)", color = "black", s = 5)
+# dates = pd.date_range(simulation_start, simulation_start + pd.Timedelta(len(model.dT) - 1, "days"))
+# n = len(dates)
+# plt.plot(dates, np.array([_.mean().astype(int) for _ in model.dT][:n]), label = "mean simulated daily cases", color = "rebeccapurple")
+# plt.fill_between(dates, [_.min().astype(int) for _ in model.dT][:n], [_.max().astype(int) for _ in model.dT][:n], label = "simulation range", alpha = 0.3, color = "rebeccapurple")
+# plt.vlines(pd.Timestamp(date), 1, 1e6, linestyles = "dashed", label = "date of seroprevalence study")
+# plt.legend(handlelength = 1, framealpha = 1)
+# plt.semilogy()
+# plt.xlim(pd.Timestamp("April 1, 2020"), dates[-1])
+# plt.ylim(1, 1e6)
+# plt.PlotDevice().xlabel("\ndate").ylabel("new daily cases\n").annotate(f"Daily Cases: Scaled Data & Simulation - Tamil Nadu (100% ve, {100 * Vf}% random assignment)")
+# plt.show()
 
 # calculate hazards
-dT  = np.array([_.mean().astype(int) for _ in model.dT])
-S   = np.array([_.mean().astype(int) for _ in model.S])
-dTx = (dT * sero_breakdown[..., None]).astype(int)
-Sx  = (S  * COVID_age_ratios[..., None]).astype(int)
-lambda_x = dTx/Sx
-Pr_covid_t     = np.zeros(lambda_x.shape)
-Pr_covid_pre_t = np.zeros(lambda_x.shape)
-Pr_covid_t[:, 0]     = lambda_x[:, 0]
-Pr_covid_pre_t[:, 0] = lambda_x[:, 0]
-for t in range(1, len(lambda_x[0, :])):
-    Pr_covid_t[:, t] = lambda_x[:, t] * (1 - Pr_covid_pre_t[:, t-1])
-    Pr_covid_pre_t[:, t] = Pr_covid_pre_t[:, t-1] + lambda_x[:, t] * (1 - Pr_covid_pre_t[:, t-1])
+# dT  = np.array([_.mean().astype(int) for _ in model.dT])
+# S   = np.array([_.mean().astype(int) for _ in model.S])
+# dTx = (dT * sero_breakdown[..., None]).astype(int)
+# Sx  = (S  * COVID_age_ratios[..., None]).astype(int)
+# lambda_x = dTx/Sx
+# Pr_covid_t     = np.zeros(lambda_x.shape)
+# Pr_covid_pre_t = np.zeros(lambda_x.shape)
+# Pr_covid_t[:, 0]     = lambda_x[:, 0]
+# Pr_covid_pre_t[:, 0] = lambda_x[:, 0]
+# for t in range(1, len(lambda_x[0, :])):
+#     Pr_covid_t[:, t] = lambda_x[:, t] * (1 - Pr_covid_pre_t[:, t-1])
+#     Pr_covid_pre_t[:, t] = Pr_covid_pre_t[:, t-1] + lambda_x[:, t] * (1 - Pr_covid_pre_t[:, t-1])
 
-plt.figure()
-for _ in range(8):
-    plt.plot(Pr_covid_pre_t[_, :], label = f"agecat:{_}")
-plt.title(f"{state}: Pr(covid before t) - Tamil Nadu, 100% ve, {100 * Vf}% random assignment")
-plt.legend()
-plt.show()
+# plt.figure()
+# for _ in range(8):
+#     plt.plot(Pr_covid_pre_t[_, :], label = f"agecat:{_}")
+# plt.title(f"{state}: Pr(covid before t) - Tamil Nadu, 100% ve, {100 * Vf}% random assignment")
+# plt.legend()
+# plt.show()
 
-plt.figure()
-for _ in range(8):
-    plt.plot(Pr_covid_t[_, :], label = f"agecat:{_}")
-plt.title(f"{state}: Pr(covid at t) - Tamil Nadu, 100% ve, {100 * Vf}% random assignment")
-plt.legend()
-plt.show()
+# plt.figure()
+# for _ in range(8):
+#     plt.plot(Pr_covid_t[_, :], label = f"agecat:{_}")
+# plt.title(f"{state}: Pr(covid at t) - Tamil Nadu, 100% ve, {100 * Vf}% random assignment")
+# plt.legend()
+# plt.show()
 # # Tx  = (T * sero_breakdown).astype(int)[..., None] + dTx.cumsum(axis = 1)
 # # Nx  = split_by_age(N)
 # # lambda_x = dTx/(Nx[..., None] - Tx)
