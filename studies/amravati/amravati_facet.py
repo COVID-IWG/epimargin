c3ff338e7f95105cc7935298a04885902d2bcefc
satej soman <satej.soman@gmail.com>
2021-04-01 20:37:06 -0500

    use smaller smoothing window for amravati

--- studies/amravati/amravati_facet.py
+++ studies/amravati/amravati_facet.py
@@ -12,7 +12,7 @@ import seaborn as sns
 
 # model details
 CI        = 0.95
-smoothing = 14
+smoothing = 7
 
 root = cwd()
 data = root/"data"
@@ -42,7 +42,7 @@ run_date     = str(pd.Timestamp.now()).split()[0]
 
 ts = get_time_series(df, ["detected_state", "detected_district"])
 
-focus = ts.loc[["Maharashtra", "Madhya Pradesh", "Gujarat"]]
+focus = ts.loc[["Maharashtra", "Madhya Pradesh", "Gujarat", "West Bengal", "Tamil Nadu"]]
 district_estimates = []
 
 for (state, district) in focus.index.droplevel(-1).unique():
@@ -71,23 +71,66 @@ for (state, district) in focus.index.droplevel(-1).unique():
     except Exception as e:
         print(e)
 
-district_estimates = pd.concat(district_estimates, axis = 0).set_index(["state", "district"])
+# handle delhi 
+delhi_ts = get_time_series(df[df.detected_state == "Delhi"], "detected_state")
+(
+    dates,
+    Rt_pred, RR_CI_upper, RR_CI_lower,
+    T_pred, T_CI_upper, T_CI_lower,
+    total_cases, new_cases_ts,
+    anomalies, anomaly_dates
+) = analytical_MPVS(delhi_ts.Hospitalized, CI = CI, smoothing = notched_smoothing(window = smoothing), totals = False)
+
+district_estimates.append(pd.DataFrame(data = {
+    "dates": dates.get_level_values(1),
+    "Rt_pred": Rt_pred,
+    "RR_CI_upper": RR_CI_upper,
+    "RR_CI_lower": RR_CI_lower,
+    "T_pred": T_pred,
+    "T_CI_upper": T_CI_upper,
+    "T_CI_lower": T_CI_lower,
+    "total_cases": total_cases[2:],
+    "new_cases_ts": new_cases_ts,
+}).assign(state = "Delhi", district = "Delhi"))
+
+district_estimates = pd.concat(district_estimates, axis = 0).set_index(["state", "district"]).sort_index()
 
 coords = { 
-    ("Madhya Pradesh", "Khandwa")   : (3, 1),
-    ("Madhya Pradesh", "Betul")     : (4, 1),
-    ("Madhya Pradesh", "Chhindwara"): (5, 1),
-    ("Madhya Pradesh", "Seoni")     : (6, 1),
-    ("Maharashtra",    "Buldhana")  : (2, 2),
-    ("Maharashtra",    "Akola")     : (3, 2),
-    ("Maharashtra",    "Amravati")  : (4, 2),
-    ("Maharashtra",    "Nagpur")    : (5, 2),
-    ("Maharashtra",    "Washim")    : (3, 3),
-    ("Maharashtra",    "Yavatmal")  : (4, 3),
-    ("Maharashtra",    "Wardha")    : (5, 3),
-    ("Maharashtra",    "Mumbai")    : (1, 4),
-    ("Maharashtra",    "Pune")      : (2, 5),
+    ("Madhya Pradesh", "Khargone")  : (4, 1),
+    ("Madhya Pradesh", "Khandwa")   : (5, 1),
+    ("Madhya Pradesh", "Betul")     : (6, 1),
+    ("Madhya Pradesh", "Chhindwara"): (7, 1),
+    ("Gujarat"       , "Surat")     : (1, 2),
+    ("Gujarat"       , "Narmada")   : (2, 2),
+    ("Maharashtra"   , "Nandurbar") : (3, 2),
+    ("Madhya Pradesh", "Barwani")   : (4, 2),
+    ("Madhya Pradesh", "Burhanpur") : (5, 2),
+    ("Maharashtra"   , "Amravati")  : (6, 2),
+    ("Maharashtra"   , "Nagpur")    : (7, 2),
+    ("Maharashtra"   , "Dhule")     : (3, 3),
+    ("Maharashtra"   , "Jalgaon")   : (4, 3),
+    ("Maharashtra"   , "Buldhana")  : (5, 3),
+    ("Maharashtra"   , "Akola")     : (6, 3),
+    ("Maharashtra"   , "Wardha")    : (7, 3),
+    ("Maharashtra"   , "Nashik")    : (3, 4),
+    ("Maharashtra"   , "Aurangabad"): (4, 4),
+    ("Maharashtra"   , "Jalna")     : (5, 4),
+    ("Maharashtra"   , "Washim")    : (6, 4),
+    ("Maharashtra"   , "Yavatmal")  : (7, 4),
+    ("Maharashtra"   , "Mumbai")    : (1, 5),
+    ("Maharashtra"   , "Thane")     : (2, 5),
+    ("Maharashtra"   , "Ahmednagar"): (3, 5),
+    ("Maharashtra"   , "Parbhani")  : (5, 5),
+    ("Maharashtra"   , "Hingoli")   : (6, 5),
+    ("Maharashtra"   , "Pune")      : (3, 6),
+    ("Maharashtra"   , "Nanded")    : (6, 6),
+    ("Delhi"         , "Delhi")     : (8, 2),
+    ("West Bengal"   , "Kolkata")   : (8, 3),
+    ("Tamil Nadu"    , "Chennai")   : (8, 4),
 }
+
+ncols = max(_[0] for _ in coords.values())
+nrows = max(_[1] for _ in coords.values())
 
 serodist = pd.read_stata("data/seroprevalence_district.dta")\
     .rename(columns = lambda _:_.replace("_api", ""))\
@@ -95,29 +138,30 @@ serodist = pd.read_stata("data/seroprevalence_district.dta")\
     .set_index(["state", "district"])
 
 yticks = {
-    "Khandwa", "Buldhana", "Washim", "Mumbai", "Pune"
+    "Surat", "Dhule", "Nashik", "Mumbai", "Pune", "Delhi", "Kolkata", "Chennai"
 }
 
-xticks = {"Buldhana", "Washim", "Yavatmal", "Wardha", "Seoni", "Mumbai", "Pune"}
+xticks = {
+    "Surat", "Narmada", "Mumbai", "Thane", "Pune", "Aurangabad", "Parbhani", "Nanded", "Yavatmal", "Chennai"
+}
 
-fig, ax_nest = plt.subplots(ncols = 6, nrows = 5)
-for (j, i) in product(range(5), range(6)):
+pop_density = pd.read_csv(data/"popdensity.csv").set_index(["state", "district"])
+fig, ax_nest = plt.subplots(ncols = ncols, nrows = nrows)
+for (j, i) in product(range(nrows), range(ncols)):
     if (i + 1, j + 1) in coords.values():
         continue
     ax_nest[j, i].axis("off")
 
 for ((state, district), (x, y)) in coords.items():
     plt.sca(ax_nest[y - 1, x - 1])
-    rural_share = int(serodist.loc[state, district]["rural_share"].mean() * 100)
+    urban_share = int((1 - serodist.loc[state, ("New " if district == "Delhi" else "") + district]["rural_share"].mean()) * 100)
+    density = pop_density.loc[state, district].density
     rt_data = district_estimates.loc[state, district].set_index("dates")["Feb 1, 2021":]
-    plt.Rt(rt_data.index, rt_data.Rt_pred, rt_data.RR_CI_upper, rt_data.RR_CI_lower, 0.95, yaxis_colors = False, ymax = 1.5)
+    plt.Rt(rt_data.index, rt_data.Rt_pred, rt_data.RR_CI_upper, rt_data.RR_CI_lower, 0.95, yaxis_colors = False, ymin = 0.5, ymax = 2.0)
     plt.gca().get_legend().remove()
     plt.gca().set_xticks([pd.Timestamp("February 1, 2021"), pd.Timestamp("March 1, 2021"), pd.Timestamp("April 1, 2021")])
-    title = f"{district} ({rural_share}%)"
-    if district == "Amravati":
-        plt.title(title)
-    else:
-        plt.title(title, alpha = 0.8)
+    plt.title(district, loc = "left", fontweight = "bold")
+    plt.title(f"({urban_share}% urban, {density}/km$^2$)", loc = "right", fontsize = 10)
     if district not in xticks:
         plt.gca().set_xticklabels([])
     if district not in yticks:
