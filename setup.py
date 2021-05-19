c5d8abe7232a94cf638162ca8d4818a4c7cc840b
satej soman <satej.soman@gmail.com>
2021-05-18 17:49:31 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='epimargin',
-    version='0.3',
-    packages=find_packages(where='.'),
+    name = 'epimargin',
+    version = '0.3',
+    packages = find_packages(where='.'),
+    author = "COVID International Working Group",
+    description = "Toolkit for estimating epidemiological metrics and evaluating public health and economic policies.",
+    url = "https://github.com/COVID-IWG/epimargin",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+    install_requires = [
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "arviz",
+        "pymc3",
+        "statsmodels",
+        "flat-table",
+        "requests",
+        "seaborn",
+        "tikzplotlib",
+        "geopandas"
+    ]
 )
